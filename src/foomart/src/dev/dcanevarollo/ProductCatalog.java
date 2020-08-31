104ff7b57dc7130cbb045e3c9aa551b84640cca0
Douglas Canevarollo <douglascanevarollo@gmail.com>
2020-08-31 15:01:01 -0300

    Implementing a WS

--- src/foomart/src/dev/dcanevarollo/ProductCatalog.java
+++ src/foomart/src/dev/dcanevarollo/ProductCatalog.java
@@ -3,8 +3,13 @@ package dev.dcanevarollo;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class ProductCatalog {
 
+	@WebMethod
 	public List<String> getProductCategories() {
 		List<String> categories = new ArrayList<>();
 
